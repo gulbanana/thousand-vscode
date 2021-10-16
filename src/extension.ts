026223332739f4d035896b1884e751ada72daca0
Thomas Castiglione <castiglione@mac.com>
2021-10-16 22:15:00 +0800

    acquire the .net runtime on-demand

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,43 @@
 import * as vscode from 'vscode';
+import * as child_process from 'child_process';
 
-export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('thousand.preview', () => {
-		vscode.window.showInformationMessage('Not yet implemented.');
+enum AcquireErrorConfiguration {
+    DisplayAllErrorPopups = 0,
+    DisableErrorPopups = 1,
+}
+
+interface IDotnetAcquireContext {
+    version: string;
+    requestingExtensionId?: string;
+    errorConfiguration?: AcquireErrorConfiguration;
+}
+
+interface IDotnetAcquireResult {
+    dotnetPath: string;
+}
+
+export async function activate(context: vscode.ExtensionContext) {
+	let acquisitionRequest: IDotnetAcquireContext =  { version: "5.0", requestingExtensionId: "gulbanana.thousand" };
+	let acquisition = vscode.commands.executeCommand<IDotnetAcquireResult>('dotnet.acquire', acquisitionRequest);
+
+	let disposable = vscode.commands.registerCommand('thousand.preview', async () => {
+		var acquisitionResult = await acquisition;
+		let dotnetPath = acquisitionResult!.dotnetPath;
+		if (!dotnetPath) {
+			vscode.window.showErrorMessage(".NET runtime installation failed.");
+			await vscode.commands.executeCommand('dotnet.showAcquisitionLog');
+			return;
+		}
+
+		let installDirectory = vscode.extensions.getExtension("gulbanana.thousand");
+		if (!installDirectory) {
+			vscode.window.showErrorMessage("Could not find installation location.");
+			return;
+		}
+
+		// XXX launch the lsp or preview generator!
+		let toolResult = child_process.spawnSync(dotnetPath, ["--info"]);
+		vscode.window.showInformationMessage(toolResult.stdout.toString());
 	});
 
 	context.subscriptions.push(disposable);
