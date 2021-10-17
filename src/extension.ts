0c7064a887dcfead1fd50f15a7b370174b1867a7
Thomas Castiglione <castiglione@mac.com>
2021-10-17 14:00:45 +0800

    language service connection & config, local-only

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,11 @@
 import * as vscode from 'vscode';
-import * as child_process from 'child_process';
+import * as fs from 'fs';
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	Trace
+  } from 'vscode-languageclient/node';
 
 enum AcquireErrorConfiguration {
     DisplayAllErrorPopups = 0,
@@ -17,30 +23,57 @@ interface IDotnetAcquireResult {
 }
 
 export async function activate(context: vscode.ExtensionContext) {
+	// activate language server
+
 	let acquisitionRequest: IDotnetAcquireContext =  { version: "5.0", requestingExtensionId: "gulbanana.thousand" };
-	let acquisition = vscode.commands.executeCommand<IDotnetAcquireResult>('dotnet.acquire', acquisitionRequest);
+	let acquisitionResult = await vscode.commands.executeCommand<IDotnetAcquireResult>('dotnet.acquire', acquisitionRequest);
 
-	let disposable = vscode.commands.registerCommand('thousand.preview', async () => {
-		var acquisitionResult = await acquisition;
-		let dotnetPath = acquisitionResult!.dotnetPath;
-		if (!dotnetPath) {
-			vscode.window.showErrorMessage(".NET runtime installation failed.");
-			await vscode.commands.executeCommand('dotnet.showAcquisitionLog');
-			return;
-		}
+	let dotnetPath = acquisitionResult!.dotnetPath;
+	if (!dotnetPath) {
+		vscode.window.showErrorMessage(".NET runtime installation failed.");
+		await vscode.commands.executeCommand('dotnet.showAcquisitionLog');
+		return;
+	}
+	
+	let serverPath = "C:\\Users\\banana\\Documents\\code\\thousand\\Thousand.LSP"; // XXX hardcoded! need to acquire this from nuget
+	if (fs.existsSync(serverPath)) {
+		let debugServer = vscode.workspace.getConfiguration("thousand.client").get("debugServer", false);
+		let extraArgs = debugServer ? ["launchDebugger"] : [];
+		let serverOptions: ServerOptions = {
+			run: { 
+				command: dotnetPath,
+				args: ["run", "-c", "Release", "-p", serverPath].concat(extraArgs)
+			},
+			debug: { 
+				command: dotnetPath,
+				args: ["run", "-p", serverPath].concat(extraArgs)
+			}
+		};
 
+		let clientOptions: LanguageClientOptions = {
+			documentSelector: [{ scheme: "file", language: "thousand" }]
+		};
+
+		let client = new LanguageClient(
+			"thousand",
+			"Thousand Words",
+			serverOptions,
+			clientOptions
+		);
+
+		context.subscriptions.push(client.start());
+	}
+
+	// register commands	
+	context.subscriptions.push(vscode.commands.registerCommand('thousand.preview', async () => {
 		let installDirectory = vscode.extensions.getExtension("gulbanana.thousand");
 		if (!installDirectory) {
 			vscode.window.showErrorMessage("Could not find installation location.");
 			return;
 		}
 
-		// XXX launch the lsp or preview generator!
-		let toolResult = child_process.spawnSync(dotnetPath, ["--info"]);
-		vscode.window.showInformationMessage(toolResult.stdout.toString());
-	});
-
-	context.subscriptions.push(disposable);
+		vscode.window.showInformationMessage("not implemented");
+	}));
 }
 
 export function deactivate() {}
