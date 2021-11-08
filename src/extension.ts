3533133cbda237176839baddc28502bb13d3b653
Thomas Castiglione <castiglione@mac.com>
2021-11-08 12:10:41 +0800

    refactor LSP init

--- src/extension.ts
+++ src/extension.ts
@@ -4,17 +4,20 @@ import { initPreviewPane } from './preview-pane';
 
 export async function activate(context: vscode.ExtensionContext) {
 	let previewCommand = vscode.commands.registerCommand("thousand.beginPreview", () => {
-        vscode.window.showInformationMessage("Preview unavailable - Thousand.LSP is loading.");
+        vscode.window.showInformationMessage("Preview unavailable - language server is loading.");
     });
 
-	let lspClient = await initLanguageService(context);
+	let output = vscode.window.createOutputChannel("Thousand Words");
+	let lspClient = await initLanguageService(context, output);
 	
 	if (lspClient != null) {
 		lspClient.onReady().then(() => initPreviewPane(context, lspClient!, previewCommand));
 	} else {
+		output.appendLine("Failed to load language server. Code completions and live previews are not available.")
+		output.show(true);
 		previewCommand.dispose();
 		context.subscriptions.push(vscode.commands.registerCommand("thousand.beginPreview", () => {
-			vscode.window.showErrorMessage("Preview unavailable - Thousand.LSP failed to load.");
+			vscode.window.showErrorMessage("Preview unavailable - language server failed to load.");
 		}));
 	}
 }
--- src/language-service.ts
+++ src/language-service.ts
@@ -25,40 +25,41 @@ interface IDotnetAcquireResult {
     dotnetPath: string;
 }
 
-export async function initLanguageService(context: vscode.ExtensionContext): Promise<LanguageClient|null> {
-	// activate language server
+export async function initLanguageService(context: vscode.ExtensionContext, output: vscode.OutputChannel): Promise<LanguageClient|null> {
+	let serverType = vscode.workspace.getConfiguration("thousand.client").get("serverType", "project");
+	let serverPath = vscode.workspace.getConfiguration("thousand.client").get("serverPath", "thousand-server"); 
+
+	// acquire .NET
 	let acquisitionRequest: IDotnetAcquireContext =  { version: "5.0", requestingExtensionId: "gulbanana.thousand" };
 	let acquisitionResult = await vscode.commands.executeCommand<IDotnetAcquireResult>('dotnet.acquire', acquisitionRequest);
 
 	let dotnetPath = acquisitionResult!.dotnetPath;
 	if (!dotnetPath) {
-		let output = vscode.window.createOutputChannel("Thousand Words");
 		output.appendLine(".NET runtime installation failed.");
 		return null;
 	}
 
-	let output = await new Promise<string>((resolve, reject) => {
-		execFile(dotnetPath, [context.asAbsolutePath("out/tool/download-server.dll")], (error, stdout, _stderr) => {
-			if (error == null) {
-				resolve(stdout);
-			} else {
-				reject(error);
-			}
+	// acquire Thousand.LSP
+	if (serverType == "internal") {
+		serverType = "assembly";
+		let download = await new Promise<string>((resolve, reject) => {
+			execFile(dotnetPath, [context.asAbsolutePath("out/tool/download-server.dll")], (error, stdout, _stderr) => {
+				if (error == null) {
+					resolve(stdout);
+				} else {
+					reject(error);
+				}
+			});
 		});
-	});
-	vscode.window.showInformationMessage(output);
+		output.append(download); // XXX don't batch this
+	} 
 	
-	// need to acquire this from nuget
-	let serverPath = vscode.workspace.getConfiguration("thousand.client").get("serverPath", "C:\\Users\\banana\\Documents\\code\\thousand\\Thousand.LSP"); 
-	let serverType = vscode.workspace.getConfiguration("thousand.client").get("serverType", "project");
-
+	// check prerequisites
 	if (serverType == "command" && !commandExists.sync(serverPath)) {
-		let output = vscode.window.createOutputChannel("Thousand Words");
-		output.appendLine("Language server not found. For more features, install https://www.nuget.org/packages/Thousand.LSP/");
+		output.appendLine("Language server not found. Change 'thousand.client.serverType' to 'internal' or install https://www.nuget.org/packages/Thousand.LSP/.");
 		return null;
 	} else if (serverType != "command" && !fs.existsSync(serverPath)) {
-		let output = vscode.window.createOutputChannel("Thousand Words");
-		output.appendLine("Language server not found at " + serverPath);
+		output.appendLine(`Language server ${serverType} not found at '${serverPath}'.`);
 		return null;
 	}
 	
@@ -76,7 +77,8 @@ export async function initLanguageService(context: vscode.ExtensionContext): Pro
 	};
 
 	let clientOptions: LanguageClientOptions = {
-		documentSelector: [{ scheme: "file", language: "thousand" }]
+		documentSelector: [{ scheme: "file", language: "thousand" }],
+		outputChannelName: "Thousand Words Log"
 	};
 
 	let client = new LanguageClient(
